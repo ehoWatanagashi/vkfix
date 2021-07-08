9377b32dc3edc98c8b0ba2feb4b9f2222b3b32e3
Artsiom Pliashkun <asperatus@inbox.ru>
2021-07-08 19:19:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+ window.onload = function() {
+	document.getElementById("fastchat-reforged").style.display = "none";
+	var elems=document.getElementsByClassName("TopNavBtn__convoList");
+	for(var i=0; i<elems.length; i++)elems[i].style.display='none';
+  };
